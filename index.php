a63c0665dd84477b1be2b703dd6d7b06a3caaf1a
Malex14 <malte.alexander.behrmann@web.de>
2018-08-12 18:15:53 +0200

    1st test

---
+++ index.php
@@ -0,0 +1,41 @@
+<html lang="de">
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="5"/>
+
+    <title>Seiten auswahl</title>
+</head>
+<body>
+    <div class="jumbotron text-center">
+        <h1>Seiten auswahl</h1>
+        <p>Diese seite Aktualisiert sich alle 5 Sekunden!</p>
+        <br>
+        <p>Benutze <kbd>F5</kbd> zum aktualisieren.</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <h3>Test 1</h3>
+            </div>
+            <div class="col-sm-3">
+                <h3>Test 2</h3>
+            </div>
+            <div class="col-sm-3">
+                <h3>Test 3</h3>
+            </div>
+            <div class="col-sm-3">
+                <h3>Tut</h3>
+                <a class="btn btn-success" href="https://www.w3schools.com/bootstrap/" role="button">Zum Tutorial</a>
+            </div>
+        </div>
+    </div> 
+</body>
+</html>
